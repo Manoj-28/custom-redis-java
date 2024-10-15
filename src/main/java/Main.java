0a03582f78bad0592a93516455b643639030716a
Manoj-28 <manojbhoye9928@gmail.com>
2024-10-15 09:32:24 +0530

    respond to multiple PINGS

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -9,14 +11,21 @@ public class Main {
         int port = 6379;
         try {
             serverSocket = new ServerSocket(port);
-            // Since the tester restarts your program quite often, setting
-            // SO_REUSEADDR ensures that we don't run into 'Address already in use'
-            // errors
-
+            //  To avoid 'address already in use' errors
             serverSocket.setReuseAddress(true);
             // Wait for connection from client.
             clientSocket = serverSocket.accept();
-            clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+            BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            OutputStream out = clientSocket.getOutputStream();
+
+            String inputLine;
+
+            while ((inputLine = reader.readLine())!= null){
+                if(inputLine.equals("PING")){
+                    out.write("+PONG\r\n".getBytes());
+                }
+            }
+
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         } finally {
