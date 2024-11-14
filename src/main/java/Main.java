002d973bca72f34dce592e7abb476068515915e7
Manoj-28 <manojbhoye9928@gmail.com>
2024-11-14 10:14:16 +0530

    handling REPLCONF in handshake

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -293,7 +293,8 @@ public class Main {
         if(isReplica && masterHost != null && masterPort > 0){
             final String finalMasterHost = masterHost;
             final int finalMasterPort = masterPort;
-            new Thread(() -> connectToMaster(finalMasterHost, finalMasterPort)).start();
+            int finalReplicaPort = port;
+            new Thread(() -> connectToMaster(finalMasterHost, finalMasterPort, finalReplicaPort)).start();
         }
 
         try (ServerSocket serverSocket = new ServerSocket(port)) {
@@ -314,14 +315,42 @@ public class Main {
         }
     }
 
-    public static void connectToMaster(String masterHost, int masterPort){
+    public static void connectToMaster(String masterHost, int masterPort, int replicaPort){
         try(Socket masterSocket = new Socket(masterHost,masterPort);
-            OutputStream out = masterSocket.getOutputStream()){
+            OutputStream out = masterSocket.getOutputStream();
+            BufferedReader in = new BufferedReader(new InputStreamReader(masterSocket.getInputStream()))){
             System.out.println("Connected to master at " + masterHost + ":" + masterPort);
             String pingCommand = "*1\r\n$4\r\nPING\r\n";
             out.write(pingCommand.getBytes());
             out.flush();
             System.out.println("Sent PING to master");
+
+            String pingResponse = in.readLine();
+            if(!"+PONG".equals(pingResponse)){
+                System.out.println("Unexpected response to PING: " + pingResponse);
+                return;
+            }
+
+            String replConfListeningPort = String.format("*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$4\r\n%d\r\n", replicaPort);
+            out.write(replConfListeningPort.getBytes());
+            out.flush();
+            System.out.println("Sent REPLCONF listening-port to master");
+
+            String replConfListeningPortResponse = in.readLine();
+            if (!"+OK".equals(replConfListeningPortResponse)) {
+                System.out.println("Unexpected response to REPLCONF listening-port: " + replConfListeningPortResponse);
+                return;
+            }
+
+            String replConfCapa = "*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n";
+            out.write(replConfCapa.getBytes());
+            out.flush();
+            System.out.println("Sent REPLCONF capa psync2 to master");
+
+            String replConfCapaResponse = in.readLine();
+            if (!"+OK".equals(replConfCapaResponse)) {
+                System.out.println("Unexpected response to REPLCONF capa psync2: " + replConfCapaResponse);
+            }
         }
         catch (IOException e){
             System.out.println("IOException when connecting to master: " + e.getMessage());
